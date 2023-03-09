ad5898a9ffadac4cbc336438bae4a6ed1466e6e6
thien1cupido <thien7028@gmail.com>
2023-03-09 11:15:40 +0700

    first commit

--- src/ss2_loop/bai_tap/CacLoaiHinh.java
+++ src/ss2_loop/bai_tap/CacLoaiHinh.java
@@ -5,18 +5,50 @@ import java.util.Scanner;
 public class CacLoaiHinh {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập chiều dài n: ");
-        int n = Integer.parseInt(scanner.nextLine());
-        System.out.print("Nhập chiều rộng m: ");
-        int m = Integer.parseInt(scanner.nextLine());
-        System.out.println("Hình chữ nhật có độ dài "+n+" và độ rộng "+m);
-        System.out.println(layHinhChuNhat(m,n));
-        System.out.print("Nhập chiều cao tam giác: ");
-        int h=Integer.parseInt(scanner.nextLine());
-        System.out.println(layHinhTamGiac(h));
-        System.out.println(layHinhTamGiacNguoc(h));
-        System.out.println(layHinhTamGiacCan(h));
-
+        int menu;
+        System.out.println("1.Hình chữ nhật " +
+                "\n2.Hình tam giác đứng " +
+                "\n3.Hình tam giác ngược" +
+                "\n4.Hình tam giác cân" +
+                "\n5.Thoát");
+        do {
+            System.out.print("Nhập thứ tự: ");
+            menu = Integer.parseInt(scanner.nextLine());
+            switch (menu) {
+                case 1:
+                    System.out.println("Hình chữ nhật ");
+                    System.out.print("Nhập chiều dài n: ");
+                    int n = Integer.parseInt(scanner.nextLine());
+                    System.out.print("Nhập chiều rộng m: ");
+                    int m = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Hình chữ nhật có độ dài " + n + " và độ rộng " + m);
+                    System.out.println(layHinhChuNhat(m, n));
+                    break;
+                case 2:
+                    System.out.println("Hình tam giác đứng ");
+                    System.out.print("Nhập chiều cao tam giác: ");
+                    int a = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Hình tam giác đứng có chiều cao " + a);
+                    System.out.println(layHinhTamGiac(a));
+                    break;
+                case 3:
+                    System.out.println("Hình tam giác ngược ");
+                    System.out.print("Nhập chiều cao tam giác: ");
+                    int b = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Hình tam giác ngược có chiều cao " + b);
+                    System.out.println(layHinhTamGiacNguoc(b));
+                    break;
+                case 4:
+                    System.out.println("Hình tam giác cân ");
+                    System.out.print("Nhập chiều cao tam giác: ");
+                    int c = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Hình tam giác cân có chiều cao " + c);
+                    System.out.println(layHinhTamGiacCan(c));
+                    break;
+                default:
+            }
+        } while (menu < 5);
+        System.out.println("Thoát");
     }
 
     public static String layHinhChuNhat(int number1, int number2) {
@@ -44,23 +76,24 @@ public class CacLoaiHinh {
     public static String layHinhTamGiacNguoc(int number) {
         String s = "";
         for (int i = 0; i < number; i++) {
-            for (int j = number-i; j >0 ;j--){
+            for (int j = number - i; j > 0; j--) {
                 s += "*";
             }
-            s+="\n";
+            s += "\n";
         }
         return s;
     }
-    public static String layHinhTamGiacCan(int number){
-        String s="";
+
+    public static String layHinhTamGiacCan(int number) {
+        String s = "";
         for (int i = 0; i < number; i++) {
-            for (int j=number-1;j>i;j--){
-                s+=" ";
+            for (int j = number - 1; j > i; j--) {
+                s += " ";
             }
-            for (int k = 0; k <(i+1)*2-1 ; k++) {
-                s+="*";
+            for (int k = 0; k < (i + 1) * 2 - 1; k++) {
+                s += "*";
             }
-            s+="\n";
+            s += "\n";
         }
         return s;
     }
